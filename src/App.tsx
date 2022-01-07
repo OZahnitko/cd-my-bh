76bed7ac82087ef2fe47feeb2183aa9d1b857c3a
sashaz <6964653+OZahnitko@users.noreply.github.com>
2022-01-07 23:09:51 +1000

    bubs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          alt="bubbs"
+          className="App-logo"
+          src="https://memegenerator.net/img/instances/61607759/ive-never-felt-so-frisky-in-my-life.jpg"
+        />
+        <h1>DECENT</h1>
       </header>
     </div>
   );
